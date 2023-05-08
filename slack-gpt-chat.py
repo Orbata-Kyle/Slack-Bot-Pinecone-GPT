824683136a50dfc1d1589a2733ec3594805e7a9a
Kyle Cords <129618831+Orbata-Kyle@users.noreply.github.com>
2023-05-08 08:43:23 -0700

    Update slack-gpt-chat.py

--- slack-gpt-chat.py
+++ slack-gpt-chat.py
@@ -10,8 +10,8 @@ SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
 
-pinecone_index = os.getenv("PINECONE_INDEX")
-pinecone_vector_length = int(os.getenv("PINECONE_VECTOR_LENGTH"))
+pinecone_index = os.getenv("PINECONE_INDEX", "your_default_index")
+pinecone_vector_length = int(os.getenv("PINECONE_VECTOR_LENGTH", 768))
 
 print("Pinecone index:", pinecone_index)
 print("Pinecone vector length:", pinecone_vector_length)
