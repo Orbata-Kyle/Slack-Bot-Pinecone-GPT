0f205586d1ca3e7f962f0a1e703f9d214887c3e4
Kyle Cords <129618831+Orbata-Kyle@users.noreply.github.com>
2023-05-09 13:36:02 -0700

    Update slack-gpt-chat.py

--- slack-gpt-chat.py
+++ slack-gpt-chat.py
@@ -1,38 +1,33 @@
-import pinecone
 import os
 import numpy as np
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import pinecone
+import openai
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv(dotenv_path="./my_secrets/DOT_ENV")  # Update the path to your DOT_ENV file
 
-SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
-client = WebClient(token=SLACK_BOT_TOKEN)
+slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
+slack_app_token = os.environ["SLACK_APP_TOKEN"]
+openai_api_key = os.environ["OPENAI_API_KEY"]
+pinecone_api_key = os.environ["PINECONE_API_KEY"]
+pinecone_index = os.environ["PINECONE_INDEX"]
+pinecone_url = os.environ["PINECONE_URL"]
+pinecone_vector_length = 768
 
-load_dotenv(dotenv_path="./my_secrets/DOT_ENV")  # Update the path to your DOT_ENV file
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_url)
 
-SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
-PINECONE_INDEX = os.environ["PINECONE_INDEX"]
-PINECONE_URL = os.environ["PINECONE_URL"]
-
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_URL)
-
-index_name = PINECONE_INDEX
-PINECONE_VECTOR_LENGTH = 768
-
-# Function to fetch memory vectors from Pinecone
 def pinecone_fetch(index_name, ids):
     index = pinecone.Index(index_name)
     fetched_vectors = index.fetch(ids=ids)
     return fetched_vectors
 
-# Function to upsert memory vectors to Pinecone
 def pinecone_upsert(index_name, items):
-    pinecone.init(api_key=PINECONE_API_KEY, environment="us-central1-gcp")
-    with pinecone.connection(api_key=PINECONE_API_KEY) as pinecone_client:
+    pinecone.init(api_key=pinecone_api_key, environment="us-central1-gcp")
+    with pinecone.connection(api_key=pinecone_api_key) as pinecone_client:
         return pinecone_client.upsert(index_name=index_name, items=items)
 
 def response_to_vector(response_text):
@@ -40,20 +35,12 @@ def response_to_vector(response_text):
     # ...
     # return the vector
 
-# Generate a memory vector for the new response
-    memory_vector = response_to_vector(response_text)
-
-# Store the memory vector in Pinecone
-    pinecone_upsert(index_name=pinecone_index, items={prompt: memory_vector})
-
 def generate_response(prompt):
-    openai.api_key = OPENAI_API_KEY
+    openai.api_key = openai_api_key
 
-    # Fetch the memory vector from Pinecone
-    memory_vector, = pinecone_fetch(index_name=PINECONE_INDEX, ids=[prompt])
+    memory_vector, = pinecone_fetch(index_name=pinecone_index, ids=[prompt])
 
     if memory_vector is None:
-        # No memory vector found, generate a new response
         response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[
@@ -63,14 +50,10 @@ def generate_response(prompt):
         )
         response_text = response.choices[0].message["content"]
 
-        # Generate a memory vector for the new response
         memory_vector = np.random.rand(pinecone_vector_length)
-
-        # Store the memory vector in Pinecone
         pinecone_upsert(index_name=pinecone_index, items={prompt: memory_vector})
 
     else:
-        # Memory vector found, generate a response using the memory vector
         response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[
@@ -84,21 +67,11 @@ def generate_response(prompt):
     return response_text
 
 
-from slack_sdk import WebClient
-
-client = WebClient(SLACK_BOT_TOKEN)
-
-# Get the bot user ID
+client = WebClient(slack_bot_token)
 bot_user_id = client.auth_test()["user_id"]
 
-import os
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-import openai
+app = App(token=slack_bot_token)
 
-app = App(token=SLACK_BOT_TOKEN)
-
-# Handler for app_mention events
 @app.event("app_mention")
 def handle_app_mention_events(body, client, logger):
     logger.info(body)
@@ -106,7 +79,6 @@ def handle_app_mention_events(body, client, logger):
     bot_mention = f"<@{bot_user_id}>"
 
     if "files" in event:
-        # Image input
         for file in event["files"]:
             if file["mimetype"].startswith("image/"):
                 image_url = file["url_private_download"]
@@ -115,25 +87,24 @@ def handle_app_mention_events(body, client, logger):
                 response = generate_response(prompt)
                 post_response(event["channel"], body, response, client)
     elif "text" in event:
-        # Text input
         text = event["text"]
         if bot_mention in text:
             text_input = text.replace(bot_mention, "").strip()
             logger.info(f"Received text input: {text_input}")
             response = generate_response(text_input)
-            post_response(event["channel"], body, response, client)
+            post_response(event["channel"],             body, response, client)
 
-# Function to post response to Slack channel
 def post_response(channel_id, body, response, client):
-    # Reply to thread 
-    response = client.chat_postMessage(channel=body["event"]["channel"], 
-                                       thread_ts=body["event"]["event_ts"],
-                                       text=f"\n{response}")
+    client.chat_postMessage(
+        channel=body["event"]["channel"], 
+        thread_ts=body["event"]["event_ts"],
+        text=f"\n{response}"
+    )
 
 if __name__ == "__main__":
     try:
-        pinecone.init(api_key=PINECONE_API_KEY)
-        handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+        pinecone.init(api_key=pinecone_api_key)
+        handler = SocketModeHandler(app, slack_app_token)
         handler.start()
     finally:
         pinecone.deinit()
