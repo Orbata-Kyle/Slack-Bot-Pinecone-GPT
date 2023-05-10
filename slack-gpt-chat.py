62127b36a08574d0dbbb3f635450166da831c667
Kyle Cords <129618831+Orbata-Kyle@users.noreply.github.com>
2023-05-09 17:29:47 -0700

    Update slack-gpt-chat.py

--- slack-gpt-chat.py
+++ slack-gpt-chat.py
@@ -7,11 +7,6 @@ import pinecone
 import openai
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-import torch
-import clip
-from PIL import Image
-import requests
-from io import BytesIO
 
 load_dotenv(dotenv_path="./my_secrets/DOT_ENV")  # Update the path to your DOT_ENV file
 
@@ -21,10 +16,9 @@ openai_api_key = os.environ["OPENAI_API_KEY"]
 pinecone_api_key = os.environ["PINECONE_API_KEY"]
 pinecone_index = os.environ["PINECONE_INDEX"]
 pinecone_url = os.environ["PINECONE_URL"]
+pinecone_vector_length = 768
 
-# Load the model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, transform = clip.load("ViT-B/32", device=device)
+pinecone.init(api_key=pinecone_api_key, environment=pinecone_url)
 
 def pinecone_fetch(index_name, ids):
     index = pinecone.Index(index_name)
@@ -32,24 +26,17 @@ def pinecone_fetch(index_name, ids):
     return fetched_vectors
 
 def pinecone_upsert(index_name, items):
+    pinecone.init(api_key=pinecone_api_key, environment="us-central1-gcp")
     with pinecone.connection(api_key=pinecone_api_key) as pinecone_client:
         return pinecone_client.upsert(index_name=index_name, items=items)
 
 def response_to_vector(response_text):
-    text_inputs = torch.tensor([clip.tokenize(response_text)]).to(device)
-    text_features = model.encode_text(text_inputs)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
-    return text_features.cpu().numpy()[0]
+    # Convert response_text to a vector
+    # ...
+    # return the vector
+    return None  # Placeholder return statement
 
-def image_to_vector(image_url):
-    response = requests.get(image_url)
-    img = Image.open(BytesIO(response.content))
-    image_input = transform(img).unsqueeze(0).to(device)
-    image_features = model.encode_image(image_input)
-    image_features /= image_features.norm(dim=-1, keepdim=True)
-    return image_features.cpu().numpy()[0]
-
-def generate_response(prompt, image_url=None):
+def generate_response(prompt):
     openai.api_key = openai_api_key
 
     memory_vector, = pinecone_fetch(index_name=pinecone_index, ids=[prompt])
@@ -64,11 +51,8 @@ def generate_response(prompt, image_url=None):
         )
 
         response_text = response.choices[0].message["content"]
-        if image_url is None:
-            memory_vector = response_to_vector(response_text)
-        else:
-            memory_vector = image_to_vector(image_url)
-        
+
+        memory_vector = np.random.rand(pinecone_vector_length)
         pinecone_upsert(index_name=pinecone_index, items={prompt: memory_vector})
 
     else:
@@ -101,7 +85,7 @@ def handle_app_mention_events(body, client, logger):
                 image_url = file["url_private_download"]
                 logger.info(f"Received image input: {image_url}")
                 prompt = f"Generate a caption for this image: {image_url}"
-                response = generate_response(prompt, image_url)
+                response = generate_response(prompt)
                 post_response(event["channel"], body, response, client)
     elif "text" in event:
         text = event["text"]
@@ -109,8 +93,8 @@ def handle_app_mention_events(body, client, logger):
             text_input = text.replace(bot_mention, "").strip()
             logger.info(f"Received text input: {text_input}")
             response = generate_response(text_input)
-            post_response(event["channel"], body, response, client)
-            
+            post_response(event["channel"],             body, response, client)
+
 def post_response(channel_id, body, response, client):
     client.chat_postMessage(
         channel=body["event"]["channel"], 
@@ -120,9 +104,8 @@ def post_response(channel_id, body, response, client):
 
 if __name__ == "__main__":
     try:
-        pinecone.init(api_key=pinecone_api_key, environment=pinecone_url)
+        pinecone.init(api_key=pinecone_api_key)
         handler = SocketModeHandler(app, slack_app_token)
         handler.start()
     finally:
         pinecone.deinit()
-
